081876c7170e5de15b87f544e8c2e87f09428389
davidpodhola <david@podhola.net>
2018-03-01 13:12:17 +0100

    more idiomatic Kotlin way

--- src/kotlin/software/hsharp/db/postgresql/provider/PgDB.kt
+++ src/kotlin/software/hsharp/db/postgresql/provider/PgDB.kt
@@ -5,7 +5,9 @@ import org.osgi.service.component.annotations.Component
 import software.hsharp.api.icommon.ICConnection
 import software.hsharp.api.icommon.IDatabase
 import software.hsharp.api.icommon.IDatabaseSetup
-import java.sql.DriverManager
+import java.sql.Driver
+import java.sql.DriverManager.registerDriver
+import java.sql.DriverManager.setLoginTimeout
 import javax.sql.DataSource
 
 @Component
@@ -17,91 +19,105 @@ open class PgDB : IDatabase
     /** Connection Timeout in seconds   */
     private val CONNECTION_TIMEOUT = 10;
 
+    protected val dataSourceObj : ComboPooledDataSource = ComboPooledDataSource()
 
-    protected val dataSource : ComboPooledDataSource = ComboPooledDataSource()
-
-	/** Driver                  */
-	private val s_driver : org.postgresql.Driver = org.postgresql.Driver()
+    /** Driver                  */
+    private val driverObj : org.postgresql.Driver = registerIfNeeded( org.postgresql.Driver() )
     private var driverRegistered : Boolean = false
 
-	/**
-	 *  Get and register Database Driver
-	 *  @return Driver
-	 */
-	override fun getDriver() : java.sql.Driver
-	{
-		if (!driverRegistered)
-		{
-			DriverManager.registerDriver (s_driver);
-			DriverManager.setLoginTimeout (CONNECTION_TIMEOUT);
-		}
-		return s_driver;
-	}   //  getDriver
+    private fun registerIfNeeded( driverInst: org.postgresql.Driver ): org.postgresql.Driver {
+        if (!driverRegistered)
+        {
+            registerDriver (driverInst);
+            setLoginTimeout (CONNECTION_TIMEOUT);
+            driverRegistered = true;
+        }
+        return driverInst;
+    }
+
+    override val dataSource: DataSource
+        get() = dataSourceObj
+    override val status: String
+        get() = doGetStatus()
+    override val driver: Driver
+        get() = driverObj
+
+    override val defaultSetupParameters : IDatabaseSetup
+        get() = PgDatabaseSetup( dataSourceName = "default", checkoutTimeout = 10, unreturnedConnectionTimeout = 10 )
 
     /**
-     * 	Create DataSource (Client)
-     *	@param connection connection
-     *	@return data dource
+     * Get Status
+     * @return status info
      */
-    override fun getDataSource() : DataSource
-    {
-        return dataSource;
-    }
-
-    /**
-	 *  Get Database Connection String.
-	 *  Requirements:
-	 *      - createdb -E UNICODE compiere
-	 *  @param connection Connection Descriptor
-	 *  @return connection String
-	 */
-    open fun getConnectionURL (connection:ICConnection ) : String
-	{
-		//  jdbc:postgresql://hostname:portnumber/databasename?encoding=UNICODE
-		val sb = StringBuilder("jdbc:postgresql://")
-			.append(connection.getDbHost())
-			.append(":").append(connection.getDbPort())
-			.append("/").append(connection.getDbName())
-			.append("?encoding=UNICODE");
-
-		return sb.toString();
-	}   //  getConnectionString
-
-    override fun connect(connection: ICConnection) {
-        dataSource.jdbcUrl = getConnectionURL(connection)
-        dataSource.user = connection.dbUid
-        dataSource.password = connection.dbPwd
-    }
-
-    override fun setup(parameters: IDatabaseSetup)
-    {
-        val params : PgDatabaseSetup = parameters as PgDatabaseSetup
-        dataSource.dataSourceName = params.dataSourceName
-        dataSource.driverClass = DRIVER
-        dataSource.preferredTestQuery = DEFAULT_CONN_TEST_SQL
-        dataSource.idleConnectionTestPeriod = params.idleConnectionTestPeriod
-        dataSource.maxIdleTimeExcessConnections = params.maxIdleTimeExcessConnections
-        dataSource.maxIdleTime = params.maxIdleTime
-        dataSource.isTestConnectionOnCheckin = params.testConnectionOnCheckin
-        dataSource.isTestConnectionOnCheckout = params.testConnectionOnCheckout
-        dataSource.acquireRetryAttempts = params.acquireRetryAttempts
-        if (params.checkoutTimeout > 0)
-            dataSource.checkoutTimeout = params.checkoutTimeout
-
-        dataSource.initialPoolSize = params.initialPoolSize
-        dataSource.initialPoolSize = params.initialPoolSize
-        dataSource.minPoolSize = params.minPoolSize
-        dataSource.maxPoolSize = params.maxPoolSize
-
-        if (params.maxStatementsPerConnection > 0)
-            dataSource.maxStatementsPerConnection = params.maxStatementsPerConnection
-
-        if (params.unreturnedConnectionTimeout > 0) {
-            dataSource.unreturnedConnectionTimeout = 1200
-            dataSource.isDebugUnreturnedConnectionStackTraces = true
+    private fun doGetStatus(): String {
+        val sb = StringBuilder()
+        try {
+            sb.append("# Connections: ").append(dataSourceObj.numConnections)
+            sb.append(" , # Busy Connections: ").append(dataSourceObj.numBusyConnections)
+            sb.append(" , # Idle Connections: ").append(dataSourceObj.numIdleConnections)
+            sb.append(" , # Orphaned Connections: ").append(dataSourceObj.numUnclosedOrphanedConnections)
+            sb.append(" , # Min Pool Size: ").append(dataSourceObj.minPoolSize)
+            sb.append(" , # Max Pool Size: ").append(dataSourceObj.maxPoolSize)
+            sb.append(" , # Max Statements Cache Per Session: ").append(dataSourceObj.maxStatementsPerConnection)
+        } catch (e: Exception) {
+            sb.append( "EXCEPTION:" + e.toString() )
         }
 
+        return sb.toString()
+    }    //	getStatus
+
+    override fun setup(parameters: IDatabaseSetup) {
+        val params : PgDatabaseSetup = parameters as PgDatabaseSetup
+        dataSourceObj.dataSourceName = params.dataSourceName
+        dataSourceObj.driverClass = DRIVER
+        dataSourceObj.preferredTestQuery = DEFAULT_CONN_TEST_SQL
+        dataSourceObj.idleConnectionTestPeriod = params.idleConnectionTestPeriod
+        dataSourceObj.maxIdleTimeExcessConnections = params.maxIdleTimeExcessConnections
+        dataSourceObj.maxIdleTime = params.maxIdleTime
+        dataSourceObj.isTestConnectionOnCheckin = params.testConnectionOnCheckin
+        dataSourceObj.isTestConnectionOnCheckout = params.testConnectionOnCheckout
+        dataSourceObj.acquireRetryAttempts = params.acquireRetryAttempts
+        if (params.checkoutTimeout > 0)
+            dataSourceObj.checkoutTimeout = params.checkoutTimeout
+
+        dataSourceObj.initialPoolSize = params.initialPoolSize
+        dataSourceObj.initialPoolSize = params.initialPoolSize
+        dataSourceObj.minPoolSize = params.minPoolSize
+        dataSourceObj.maxPoolSize = params.maxPoolSize
+
+        if (params.maxStatementsPerConnection > 0)
+            dataSourceObj.maxStatementsPerConnection = params.maxStatementsPerConnection
+
+        if (params.unreturnedConnectionTimeout > 0) {
+            dataSourceObj.unreturnedConnectionTimeout = 1200
+            dataSourceObj.isDebugUnreturnedConnectionStackTraces = true
+        }
     }
+
+    override fun connect(connection: ICConnection) {
+        dataSourceObj.jdbcUrl = getConnectionURL(connection)
+        dataSourceObj.user = connection.dbUid
+        dataSourceObj.password = connection.dbPwd
+    }
+
+    /**
+     *  Get Database Connection String.
+     *  Requirements:
+     *      - createdb -E UNICODE compiere
+     *  @param connection Connection Descriptor
+     *  @return connection String
+     */
+    open fun getConnectionURL (connection:ICConnection ) : String
+    {
+        //  jdbc:postgresql://hostname:portnumber/databasename?encoding=UNICODE
+        val sb = StringBuilder("jdbc:postgresql://")
+                .append(connection.dbHost)
+                .append(":").append(connection.dbPort)
+                .append("/").append(connection.dbName)
+                .append("?encoding=UNICODE");
+
+        return sb.toString();
+    }   //  getConnectionString
 
     /**
      * String Representation
@@ -109,47 +125,17 @@ open class PgDB : IDatabase
      */
     override fun toString(): String {
         val sb = StringBuilder("DB_PostgreSQL[")
-        try {
-            val logBuffer = StringBuilder(50)
-            logBuffer.append("# Connections: ").append(dataSource.numConnections)
-            logBuffer.append(" , # Busy Connections: ").append(dataSource.numBusyConnections)
-            logBuffer.append(" , # Idle Connections: ").append(dataSource.numIdleConnections)
-            logBuffer.append(" , # Orphaned Connections: ").append(dataSource.numUnclosedOrphanedConnections)
-        } catch (e: Exception) {
-            sb.append("=").append(e.localizedMessage)
-        }
-
+        sb.append(doGetStatus())
         sb.append("]")
         return sb.toString()
     }   //  toString
 
-    /**
-     * Get Status
-     * @return status info
-     */
-    override fun getStatus(): String? {
-        val sb = StringBuilder()
-        try {
-            sb.append("# Connections: ").append(dataSource.numConnections)
-            sb.append(" , # Busy Connections: ").append(dataSource.numBusyConnections)
-            sb.append(" , # Idle Connections: ").append(dataSource.numIdleConnections)
-            sb.append(" , # Orphaned Connections: ").append(dataSource.numUnclosedOrphanedConnections)
-            sb.append(" , # Min Pool Size: ").append(dataSource.minPoolSize)
-            sb.append(" , # Max Pool Size: ").append(dataSource.maxPoolSize)
-            sb.append(" , # Max Statements Cache Per Session: ").append(dataSource.maxStatementsPerConnection)
-        } catch (e: Exception) {
-        }
-
-        return sb.toString()
-    }    //	getStatus
-
-
     fun getNumBusyConnections() : Int {
-        return dataSource.numBusyConnections
+        return dataSourceObj.numBusyConnections
     }
 
     fun getJdbcUrl() : String {
-        return dataSource.jdbcUrl
+        return dataSourceObj.jdbcUrl
     }
 
     /**
@@ -158,10 +144,8 @@ open class PgDB : IDatabase
     open fun close() {
 
         try {
-            dataSource.close()
+            dataSourceObj.close()
         } catch (e: Exception) {
-            e.printStackTrace()
         }
     }    //	close
-
 }
